1c588a3c05dd23e84f9a3368ade4091415d2685b
Barath Senthil <barath.skpf29@gmail.com>
2017-08-23 18:59:31 +0530

    - Included a function with return values for calculating the net salary amount for each month - Added a function with default arguments to determine the gender of a person
    - created a commented heading to describe the main section for calling the function

--- Functions.py
+++ Functions.py
@@ -6,7 +6,30 @@ def eurtoinr(euros):
     rupees = euros * 75
     print (rupees)
 
+# function with return values
+def netsalary (monthlygross):
+    tax = monthlygross * 0.40
+    netamount = monthlygross - tax
+    return netamount
 
+# function with default arguments
+def get_gender(sex = 'Unknown'):
+    if sex is 'm':
+        sex = 'male'
+    elif sex is 'f':
+        sex = 'female'
+    print (sex)
+
+
+
+#-------------------main----------------------
 # calling the function
 greetings()
 eurtoinr(150)
+
+salary_in_hand = netsalary(4000)
+print "The salary i would get every month", salary_in_hand, "euros"
+
+get_gender('m')
+get_gender('f')
+get_gender()
