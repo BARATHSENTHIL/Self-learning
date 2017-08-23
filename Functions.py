b87eb12776f23e24ab587111a618fa0c72a5d499
Barath Senthil <barath.skpf29@gmail.com>
2017-08-23 18:00:12 +0530

    - Added new python file to understand about functions
    - Included the code snippet for defining and calling the function (function without parameters and function with parameters)

---
+++ Functions.py
@@ -0,0 +1,12 @@
+# function definition
+def greetings():
+    print("Hey, I am Barath")
+
+def eurtoinr(euros):
+    rupees = euros * 75
+    print (rupees)
+
+
+# calling the function
+greetings()
+eurtoinr(150)
