657dc38fa15112f84d03102e39c176cd6945a28f
Barath Senthil <barath.skpf29@gmail.com>
2017-08-26 18:48:28 +0530

    Created a new class called Calculator to add and mul two variables
    - The class included __init__() method which behaves as a constructor of a class to initialise its member variables

--- Classes.py
+++ Classes.py
@@ -1,9 +1,18 @@
-class Greetings:
-    count = 0
+class Calculator:
 
-    def Introduce(self):
-        print "Hi I am barath. Warm wishes"
-        self.count += 1
+    # The init method is similar to constructor of a class which is used to initialize the member variables
+    def __init__(self, x, y):
+        self.operand1 = x
+        self.operand2 = y
 
-h = Greetings()
-h.Introduce()
+    def add(self):
+        print self.operand1 + self.operand2
+
+    def mul(self):
+        print self.operand1 * self.operand2
+
+cal_1 = Calculator(10, 20)
+cal_1.add()
+
+cal_2 = Calculator(110, 230)
+cal_2.mul()
