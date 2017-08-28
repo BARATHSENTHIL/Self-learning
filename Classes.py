fcda39a63ad61e8631cf792f7d66a141bccfc06b
Barath Senthil <barath.skpf29@gmail.com>
2017-08-29 00:44:41 +0530

    Included example code to identify min, max and sorting the dictionary elements

--- Classes.py
+++ Classes.py
@@ -15,3 +15,24 @@ stud2 = Student('Karthik')
 print stud1.studentname,  stud1.University
 print stud2.studentname,  stud2.University
 
+#------------Sorting a Dictionary---------------
+Stocks = {
+    'GOOG': 520.54,
+    'FB': 76.45,
+    'YHOO': 39.28,
+    'AMZN': 306.21,
+    'AAPL': 99.76
+}
+
+# The zip function is used to sort the dictionary
+
+# Min and Max values in dictionary
+print(min(zip(Stocks.values(), Stocks.keys())))
+
+print(max(zip(Stocks.values(), Stocks.keys())))
+
+# Sorting the elements in dictionary based on values
+print(sorted(zip(Stocks.values(), Stocks.keys())))
+
+# Sorting the elements in dictionary based on keys
+print(sorted(zip(Stocks.keys(), Stocks.values())))
